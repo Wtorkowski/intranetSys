ce49563ddc12b0bf677850a19e184a06936185b5
Bartlomiej <wtoras@gmail.com>
2022-02-20 18:13:17 +0100

    added first catalog

---
+++ sur/src/main/java/com/technisat/sur/SurApplication.java
@@ -0,0 +1,13 @@
+package com.technisat.sur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurApplication.class, args);
+	}
+
+}
---
+++ sur/src/test/java/com/technisat/sur/SurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technisat.sur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
